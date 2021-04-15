adec26fc05e0e2e6c13b4c0c996e11146f580cb5
ring.lin <ring.lin@huolala.cn>
2021-04-15 20:09:17 +0800

    fixed 导入数据表名前缀

--- database/migrations/820210415163016_store_user.php
+++ database/migrations/820210415163016_store_user.php
@@ -45,4 +45,19 @@ class StoreUser extends Migrator
 			->addIndex(['created_at'], ['name' => 'ix_created_at'])
             ->create();
     }
+
+    public function up()
+    {
+        $singleRow = [
+            'id' => 1,
+            'username' => 'admin',
+            'password' => 'ade43e3ae3c2013b7b76026c8f0ea9c334b0d6a7',
+            'phone' => '12345678901',
+            'realname' => '石头',
+            'is_super' => 1,
+            'is_lock' => 0,
+            'is_delete' => 0
+        ];
+        $this->table('store_user')->insert($singleRow)->saveData();
+    }
 }
