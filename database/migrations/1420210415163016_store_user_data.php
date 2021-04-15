6cb47239acc17ab76f059b227d249280645174f4
ring.lin <ring.lin@huolala.cn>
2021-04-15 20:12:03 +0800

    fixed 导入数据表名前缀

---
+++ database/migrations/1420210415163016_store_user_data.php
@@ -0,0 +1,23 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class StoreUserData extends Migrator
+{
+    public function up()
+    {
+        $singleRow = [
+            'id' => 1,
+            'username' => 'admin',
+            'password' => 'ade43e3ae3c2013b7b76026c8f0ea9c334b0d6a7',
+            'phone' => '12345678901',
+            'realname' => '石头',
+            'is_super' => 1,
+            'is_lock' => 0,
+            'is_delete' => 0
+        ];
+        $this->table('store_user')->insert($singleRow)->saveData();
+    }
+}
